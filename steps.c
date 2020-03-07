a1750e8edafb5dec537ccc944bfb5db664dd9e96
Birkan Düzel <56878577+duezelbi@users.noreply.github.com>
2020-03-07 14:11:24 +0100

    added forces to steps.c, corrected boundaries in for loops for action(), changed tau/s to tau/2 in step_md; small formatting improvements?

--- steps.c
+++ steps.c
@@ -24,17 +24,17 @@ double action(double *h, parameters params) {
 
     double s = 0.0; // total sum
 
-    for (int p=0; p<P; p++) {
+    for (int p=0; p<(P-1); p++) {
         double s1 = 0.0; // first partial sum
         double s2 = 0.0; // second partial sum
 
-        for (int n=0; n<N; n++) {
+        for (int n=0; n<(N-1); n++) {
             double diff = h[tup2ind(n, p+1, N, P)] - h[tup2ind(n, p, N, P)];
             s1 += diff*diff;
         }
         s1 *= P/(beta*2);
 
-        for (int n=1; n<N; n++) {
+        for (int n=1; n<(N-1); n++) {
             double diff = h[tup2ind(n, p, N, P)] - h[tup2ind(n-1, p, N, P)];
             s2 += diff*diff;
         }
@@ -55,6 +55,34 @@ void force(double *f, double *h, parameters params) {
     output:
         double *f: forces (N*P matrix)
     */
+    unsigned int N = params.N;
+    unsigned int P = params.P;
+    double v0 = params.v0;
+    double beta = params.beta;
+    unsigned int size = N * P;
+
+    for (int n=0; n<(N-1); n++) {
+        for (int p=0; p<(P-1); p++) {
+            double a1 = 0.0; // summand A(n,p)
+            double b1 = 0.0; // summand B(n,p)
+            if (p==0) {
+                a1 = P/beta * (h[tup2ind(n, 1, N, P)] - h[tup2ind(n, 0, N, P)]);
+            }
+            else {
+                a1 = P/beta * (h[tup2ind(n, p+1, N, P)] + h[tup2ind(n, p-1, N, P)] - 2 * h[tup2ind(n, p, N, P)]);
+            }
+            if (n==0) {
+                b1 = beta * v0 * v0/P * (h[tup2ind(1, p, N, P)] - 2 * h[tup2ind(0, p, N, P)]);
+            }
+            else if (n==(N-1)) {
+                b1 = beta * v0 * v0/P * (h[tup2ind(N-2, p, N, P)] - h[tup2ind(N-1, p, N, P)]);
+            }
+            else {
+                b1 = beta * v0 * v0/P * (h[tup2ind(n+1, p, N, P)] + h[tup2ind(n-1, p, N, P)] - 2 * h[tup2ind(0, p, N, P)]);
+            }
+            f[tup2ind(n, p, N, P)]= a1 + b1;
+        }
+    }
 }
 
 double hamiltonian(double *p, double *h, parameters params) {
@@ -113,14 +141,14 @@ void step_md(double *p, double *h, parameters params) {
     for (int m=0; m<M; m++) {
         // step 1
         force(f, h, params);
-        vec_scalar_vec_add(p, tau/s, f, p, N*P);
+        vec_scalar_vec_add(p, tau/2, f, p, N*P);
 
         //step 2
         vec_scalar_vec_add(h, tau, p, h, N*P);
 
         //step 3
         force(f, h, params);
-        vec_scalar_vec_add(p, tau/s, f, p, N*P);
+        vec_scalar_vec_add(p, tau/2, f, p, N*P);
     }
 
 }
@@ -142,14 +170,14 @@ int step_mc(double *p, double *h, gsl_rng * r, parameters params) {
 	double r_max = gsl_rng_max(r);
 	double H_0 = hamiltonian(p, h, params);
 	unsigned int N = params.N;
-   	unsigned int P = params.P;
+  unsigned int P = params.P;
 	unsigned int size = N * P;
 
 	double *p0 = malloc(size * sizeof(double));
 	double *h0 = malloc(size * sizeof(double));
     // copy the state to reset the config, if it's not accepted after the step
-    memcpy(p0, p, size*sizeof(double));
-    memcpy(h0, h, size*sizeof(double));
+  memcpy(p0, p, size*sizeof(double));
+  memcpy(h0, h, size*sizeof(double));
 
 	step_md(p, h, params);
 
@@ -162,15 +190,17 @@ int step_mc(double *p, double *h, gsl_rng * r, parameters params) {
 		if (gsl_rng_get (r)/r_max > prob) {
             // the random number is higher than the calculated probability, reject the step
             // copy the old configuration into the "output"
-            memcpy(p, p0, size*sizeof(double));
+      memcpy(p, p0, size*sizeof(double));
 			memcpy(h, h0, size*sizeof(double));
 			return 0;
-		} else {
+		}
+    else {
             // the random number is smaller, accept the step
             return 1;
-        }
-	} else {
+    }
+	}
+  else {
         // the new energy is smaller, accept the step
         return 1;
-    }
+  }
 }
