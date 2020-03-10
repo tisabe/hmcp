4b0d03f6acf2d60890593010f8bf113d2d5df4c0
IDungeon <57952984+IDungeon@users.noreply.github.com>
2020-03-10 17:13:38 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "structs.h"
+#include "utils.h"
+#include "init_config.h"
+
+
+int main() {
+    
+    int time_max = 50;
+
+    parameters params;
+    params.M = 100;
+    params.N = 10;
+    params.v0 = 0.2;
+    params.beta = 0.7;
+    params.P = 50;
+    params.size = params.P * params.N;
+
+    double *h = malloc(params.size*sizeof(double));
+    double *h_out = malloc(params.size*sizeof(double));
+    double *p = malloc(params.size*sizeof(double));
+    double *p_out = malloc(params.size*sizeof(double));
+    int *acceptance = malloc(time*sizeof(int));
+
+    init_config_rng(h, 42, params);
+    
+    for (unsigned int time=1; time<=time_max; time++) {
+	gsl_rng * r = gsl_rng_alloc (gsl_rng_taus);
+	gsl_rng_set(r, time); //use time as seed
+
+
+	heat_bath(p, r, params);
+        
+        acceptance[time] = step_mc(p, h, r, params);
+
+		if (acceptance[time]==1){
+        		printf("step %e got accepted", time);
+		} elseif (acceptance[time]==0){
+ 			printf("step %e got not accepted", time);
+		}
+    }
+
+    return 0;
+}
