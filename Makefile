9478e710ed7c8e521ef7f2d1472c1b3c61fda4af
Birkan Düzel <56878577+duezelbi@users.noreply.github.com>
2020-03-11 15:08:49 +0100

    creation and testing of observables.c

--- Makefile
+++ Makefile
@@ -5,3 +5,7 @@ main: geometry.c vmath.c metropolis.c init_config.c
 test_md: utils.c init_config.c steps.c
 	gcc test_md.c -lm init_config.c utils.c steps.c -lgsl -o test_md.exe
 	./test_md.exe
+
+observables: utils.c
+	gcc observables.c -lm utils.c -o observables.exe
+	./observables.exe
---
+++ observables.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "structs.h"
+#include "utils.h"
+
+double potential_energy(double *h, parameters params) {
+    double V;
+    V = 0;
+
+    for (int p=0; p<params.P; p++) {
+        V += params.v0 * params.v0 / 2 *(h[tup2ind(1,p,params.N,params.P)]*h[tup2ind(1,p,params.N,params.P)]);
+        for (int n=0; n<params.N) {
+            V += (h[tup2ind(n,p,params.N,params.P)]-h[tup2ind(n-1,p,params.N,params.P)]) * (h[tup2ind(n,p,params.N,params.P)]-h[tup2ind(n-1,p,params.N,params.P)]);
+        }
+    }
+    V = V/ params.P;
+    return V;
+}
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
