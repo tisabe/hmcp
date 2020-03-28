a54eff7493bdbb8226b93605370651a37113ca8a
IDungeon <57952984+IDungeon@users.noreply.github.com>
2020-03-28 15:53:59 +0100

    Update init_config.c

--- init_config.c
+++ init_config.c
@@ -5,28 +5,26 @@
 
 #include "structs.h"
 /*********************************************************************************
-init_config_rng: this function creates an array of length "size" with random entries between 0 and 1.
+init_config_rng: this function initializes random entries between 0 and 1 in the array "configuration".
 		input: *configuration: configuration of length "size"
 			r: initialized random number generator
 			N: number of atoms
 			P: length of the interval for the discretized auxiliary continuous coordinate
 
-init_zero: this function creates an array with all entries equal to 0.
+init_zero: this function initializes all entries to 0 in the array "configuration".
 		input: *configuration: configuration of length "size"
 			N: number of atoms
 			P: length of the interval for the discretized auxiliary continuous coordinate
 **********************************************************************************/
 
 void init_config_rng(double *configuration, gsl_rng * r, parameters params){
-	unsigned int size= params.N * params.P;
-    	for (unsigned int i=0; i<size; i++) {
+    	for (unsigned int i=0; i<params.size; i++) {
 		configuration[i] = gsl_rng_uniform(r);
     	}
 }
 
 void init_zero(double *configuration, parameters params) {
-	unsigned int size = params.N*params.P;
-	for (unsigned int i=0; i<size; i++) {
+	for (unsigned int i=0; i<params.size; i++) {
 		configuration[i] = 0.0;
 	}
 }
