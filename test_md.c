703d46d7397454791bad6ef617e62ae6df6d0999
IDungeon <57952984+IDungeon@users.noreply.github.com>
2020-03-29 19:00:10 +0200

    Update test_md.c

--- test_md.c
+++ test_md.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <gsl/gsl_rng.h>
 
 #include "steps.h"
 #include "structs.h"
@@ -21,12 +22,11 @@ int main() {
     params.v0 = 0.2;
     params.beta = 0.7;
     params.P = 50;
-	params.size = 10*50;
+    params.size = 10*50;
     double *h = malloc(params.N*params.P*sizeof(double));
     double *h_out = malloc(params.N*params.P*sizeof(double));
     double *p = malloc(params.N*params.P*sizeof(double));
     double *p_out = malloc(params.N*params.P*sizeof(double));
-
     gsl_rng * r = gsl_rng_alloc (gsl_rng_taus);
     gsl_rng_set(r, 42);
 
@@ -35,12 +35,13 @@ int main() {
     for (unsigned int m=10; m<=max_M; m+=10) {
         params.M = m; // update the number of leapfrog steps
         init_zero(h, params); // initialize the configuration to zero
-        init_config_rng(p, r, params); //rng needs a seed atm
+    	init_config_rng(p, r, params);
         double ham = hamiltonian(p, h, params); // calculate the prior hamiltonian
+
         step_md(p, h, params); // perform the md simulation
         ham = hamiltonian(p, h, params) - ham;
         printf("tau = %e, delta_H = %e\n", 1/(float)m, ham);
-        fprintf(obs_file, "%e\t%e\n", 1/(float)m, ham);
+	fprintf(obs_file, "%e\t%e\n", 1/(float)m, ham);
     }
     fclose(obs_file);
     free(h);
