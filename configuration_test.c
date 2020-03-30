3eb54ba8669b8b7f6bf12de32fc29fa665ed5ea4
tisabe <bechtelt@physik.hu-berlin.de>
2020-03-30 20:57:47 +0200

    made executables executable

--- configuration_test.c
+++ configuration_test.c
@@ -7,12 +7,16 @@
 #include "observables.h"
 
 int main(){
+  printf("Testing if analytical solution agrees with numerical result for all h = 2\n");
+
   parameters params;
   params.N = 10;
   params.P = 5;
   params.size = params.N * params.P;
   params.beta = 0.7;
   params.v0 = 0.2;
+
+  printf("N = %d, P = %d, size = %d, beta = %f, v0 = %f\n", params.N, params.P, params.size, params.beta, params.v0);
 
 
   double *h = malloc(params.size*sizeof(double));
@@ -24,6 +28,8 @@ int main(){
   double potential_of_2 = potential_energy(h,params);
 
   printf("\n action: %f \t potential: %f \n", action_of_2, potential_of_2);
+
+  printf("Difference to analytical result: action: %e \t potential: %e\n", action_of_2 - 0.056, potential_of_2 - 0.08);
 
   free(h);
 
